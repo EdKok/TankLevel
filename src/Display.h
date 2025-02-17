21dbc2c3e5308c0855db9b1a15deaf0a8aefe648
Ed Kok <ed.kok@upcmail.nl>
2025-02-17 07:51:55 +0100

    Made needle move smoother

---
+++ src/Display.h
@@ -0,0 +1,56 @@
+#include "SPI.h"
+#include "U8g2lib.h"
+
+U8G2_SSD1309_128X64_NONAME0_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 15, /* dc=*/ 2, /* reset=*/ 4);
+
+byte cx=64;           // x center
+byte cy=45;           // y center
+byte radius=40;       // radius outer circle
+byte radius2=30;      // radius inner circle
+byte radius3=6;       // radius needle mount
+int value;
+int skip = 0;
+
+void Display_function(float output, bool is_connected) {
+  u8g2.clearBuffer();				                  // clear the internal memory
+  u8g2.setFont(u8g2_font_unifont_h_symbols);  // just for the up/down arrows
+
+  value = (int(output));                      // cast float to int
+  if (skip<2) {                               // Prevent needle starting at "0" first round,
+    skip++;                                   // skips first iterations
+    return;
+  }
+
+  if (value<1) { value = 0; }                 // prevent needle below '0'
+  
+  u8g2.setFont(u8g2_font_t0_12b_te);
+  u8g2.drawCircle(cx,cy,radius, U8G2_DRAW_UPPER_LEFT|U8G2_DRAW_UPPER_RIGHT );
+  u8g2.drawCircle(cx,cy,radius2, U8G2_DRAW_UPPER_LEFT|U8G2_DRAW_UPPER_RIGHT );
+  u8g2.drawCircle(cx,cy,radius3, U8G2_DRAW_UPPER_LEFT|U8G2_DRAW_UPPER_RIGHT );
+  u8g2.drawLine(cx-radius, cy, cx-radius2, cy);
+  u8g2.drawLine(cx+radius, cy, cx+radius2, cy);
+  u8g2.drawLine(cx, cy-radius, cx, cy-radius2-6);   // 50% 
+  u8g2.drawLine(cx+radius * sin(3.14 * 2 * 315/360)+1, cy-radius * cos(3.14 * 2 * 315 / 360)+1 , (cx+radius2 * sin(3.14 * 2 * 315/360))-4, (cy-radius2 * cos(3.14 * 2 * 315/360))-4);   // 25%
+  u8g2.drawLine(cx+radius * sin(3.14 * 2 * 45/360), cy-radius * cos(3.14 * 2 * 45 / 360) , (cx+radius2 * sin(3.14 * 2 * 45 / 360))+5, (cy-radius2 * cos(3.14 * 2 * 45 / 360))-5);       // 75%
+  u8g2.setFont(u8g2_font_t0_11_tn);
+  u8g2.drawStr(12,48,"0"); u8g2.drawStr(110,48,"100");
+  u8g2.setFont(u8g2_font_t0_13b_te);
+  u8g2.drawStr(2,64,"Vuilwater:");
+  u8g2.setCursor(72,64); u8g2.print(value); u8g2.print("% vol");
+  if (value>85) { u8g2.drawStr(0,10,"VOL!"); }
+  else if (value>80) { u8g2.drawStr(0,10,"Bijna"); u8g2.drawStr(0,20,"vol"); }
+  else if (value<5) { u8g2.drawStr(0,10,"Leeg"); }
+  if (is_connected) { u8g2.drawStr(110,10,"OK"); }
+  else { u8g2.drawStr(105,10,"NOK"); }
+  float val = map(value, 0, 100, 0, 180);
+  val = val*3.14/180 -1.572;
+  int xp = cx+(sin(val) * radius);
+  int yp = cy-(cos(val) * radius);
+  u8g2.drawLine(cx,cy,xp,yp);
+  u8g2.drawLine(cx-1,cy-1,xp,yp);
+  u8g2.drawLine(cx+1,cy-1,xp,yp);
+  u8g2.drawLine(cx-1,cy,xp,yp);
+  u8g2.drawLine(cx+1,cy,xp,yp);
+
+  u8g2.sendBuffer();					            // transfer internal memory to the display
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,116 @@
 // Tank levels are supposed to be sent to Signal K as a "ratio" - a number between 0 and 1, where 0 is empty and 1 is full
-// Sensors covered with glass or lens will need to be calibrated
-#include <Wire.h>                               // IIC
-#include "Adafruit_VL53L0X.h"
+// Sensors covered with glass or a lens will need to be calibrated
+
+#include "Wire.h"                               // IIC for sensor
+#include "Display.h"                            // Separate file for outputting to display
+#include "splash.h"                             // Show splash screen at startup
+#include "Adafruit_VL53L0X.h"                   // The driver for the sensor
 #include "sensesp/signalk/signalk_output.h"
 #include "sensesp_app_builder.h"
 #include "sensesp/transforms/moving_average.h"
 #include "sensesp/transforms/linear.h"
 #include "sensesp/transforms/lambda_transform.h"
+#include "sensesp/net/ws_client.h"
 
 using namespace sensesp;
 
 reactesp::ReactESP app;
 
 Adafruit_VL53L0X mySensor = Adafruit_VL53L0X();
-uint delayTime = 1000;
+uint delayTime = 2000;
 SKOutput<float>* distance_output;
 const char* sensor_config_path = "/sensors/vl53l0x/distance";
 const char* sk_path = "tanks.blackwater.level";
+bool is_connected = true;                        // Assume connected to SKserver by default
 
-float read_distance_callback() { return (mySensor.readRange()); }
-int Height = 450; //Height of tank in mm
+int read_distance_callback() {
+    int distance = mySensor.readRange();
+    Serial.print("Distance: "); Serial.println(distance); // Print the distance
+    return distance;
+}
 
 void setup() {
-  Serial.begin(115200);
-  delay(100);
+    Serial.begin(115200);
+    delay(100);
 
-  if (!mySensor.begin()) {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
-  } else {
-      Serial.println(F("Adafruit VL53L0X started"));
-  }
-  mySensor.setMeasurementTimingBudgetMicroSeconds(200000);
+    // Initialize display driver and show splash screen
+    u8g2.begin();
+    u8g2.firstPage();
+    do {
+        u8g2.drawXBMP(0, 0, 128, 64, u8g2_logo);  // Show splash during startup
+    } while (u8g2.nextPage());
 
-  SensESPAppBuilder builder;
-  sensesp_app = builder.set_hostname("SensESP BlackWaterLevel")
-                    ->set_wifi("", "")
-                    ->set_sk_server("10.10.10.1", 3000)
-                    ->enable_ota("myOTApwd")
-                    // ->enable_wifi_signal_sensor()
-                    ->get_app();
+    // Initialize the sensor and handle failure
+    if (!mySensor.begin()) {
+        Serial.println(F("Failed to init VL53L0X"));
+        while (1) {
+            delay(100);  // Add a delay to avoid busy-waiting
+        }
+    } else {
+        Serial.println(F("Adafruit VL53L0X started"));
+    }
 
-auto Lambda_function = [](int input) ->float{
-  if (input <=8) { input = 0; }
-  int Diff = (Height - input);
-  float output = (float(Diff) / float(Height));
-  return output;
-};
+    // Set high accuracy for the sensor
+    mySensor.setMeasurementTimingBudgetMicroSeconds(200000);
 
-auto Lambda_transform = new LambdaTransform<int, float>(Lambda_function);
-auto* tanklevel = new RepeatSensor<int>(delayTime, read_distance_callback);           // returns mm
-      tanklevel->connect_to(new Linear(1.0, -32, "/BlackWaterLevel/Linear"))          // mutiplier, offset
-               ->connect_to(Lambda_transform)                                         // calculate ratio (0....1)
-               ->connect_to(new SKOutputFloat(sk_path));
+    // Build the SensESP application
+    SensESPAppBuilder builder;
+    sensesp_app = builder.set_hostname("SensESP BlackWaterLevel")
+                         ->set_wifi("AP-SSID", "SomePWD")
+                         ->set_sk_server("10.10.10.1", 3000)
+                         ->enable_ota("myOTApwd")
+                         //->enable_wifi_signal_sensor()
+                         ->get_app();
 
-  sensesp_app->start();
+    // Lambda function to calculate the tank level ratio
+    int oldOutput = 0;                                  // Initialize oldOutput
+    bool firstRun = true;                               // Flag to check if it's the first run
+
+    auto Lambda_function = [oldOutput, firstRun](int input, int deadzone, int height) mutable -> float {
+        if (input <= deadzone) { input = 0; }
+        if (input > height) { input = height; }
+        int output = (height - input) * 100 / height;   // From ratio to percent and convert to integer
+
+        if ((firstRun) && (output > 0)) {               // Check if it's the first run and the output is greater than 0
+            oldOutput = output;                         // Initialize oldOutput with the actual value on the first run
+            firstRun = false;                           // Set the flag to false after the first run
+        } else {
+            if (output > oldOutput) {          
+                output = oldOutput + 1;
+                if (output > 100) { output = 100; }
+            } else if (output < oldOutput) {
+                output = oldOutput - 1;
+                if (output < 0) { output = 0; }
+            }
+        }
+
+        oldOutput = output;                              // Save the actual value for the next run
+
+    // Parameters for the lambda transform
+    const ParamInfo* lambda_param_data = new ParamInfo[2]{
+        {"deadzone", "DeadZone of sensor in mm"}, {"height", "Height of tank in mm"}
+    };
+
+    // Create the lambda transform
+    auto Lambda_transform = new LambdaTransform<int, float, int, int>(
+         Lambda_function, 46, 400, lambda_param_data, "/BlackWaterLevel/Params"
+    );
+
+    // Create the tank level sensor and connect the transforms
+    auto* tanklevel = new RepeatSensor<int>(delayTime, read_distance_callback);             // returns mm
+
+          tanklevel->connect_to(new Linear(1, 0, "/BlackWaterLevel/Linear"))                // mutiplier (1), offset (-30)
+                   ->connect_to(Lambda_transform)                                           // calculate ratio (0....1)
+                   ->connect_to(new SKOutputFloat(
+                    sk_path,                                                                // Signal K path
+                    new SKMetadata("ratio",                                                 // Define output units
+                                   "Blackwater Tank Level")                                 // Value description
+                   ));
+                            
+    // Start the SensESP application
+    sensesp_app->start();
 }
 
 void loop() {
-  app.tick();
+    app.tick();
 }
